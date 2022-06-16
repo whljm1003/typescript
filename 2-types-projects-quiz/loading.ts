935247e1233a4ee5602696aa7db80d92bfa5058b
Jungmin Lee <whljm1003@gmail.com>
2022-06-16 15:20:23 +0900

    2-loading

--- 2-types-projects-quiz/loading.ts
+++ 2-types-projects-quiz/loading.ts
@@ -3,24 +3,50 @@
    * Print Loading State
    */
   type LoadingState = {
-    state: 'loading';
+    state: "loading";
   };
 
   type SuccessState = {
-    state: 'success';
+    state: "success";
     response: {
       body: string;
     };
   };
 
   type FailState = {
-    state: 'fail';
+    state: "fail";
     reason: string;
   };
 
   type ResourceLoadState = LoadingState | SuccessState | FailState;
+  // My solution
+  function printLoginState21(state: ResourceLoadState) {
+    if (state.state === "loading") {
+      console.log("👀 loading...");
+    } else if (state.state === "success") {
+      console.log(`😃 ${state.response.body}`);
+    } else if (state.state === "fail") {
+      console.log(`😱 ${state.reason}`);
+    }
+  }
 
-  printLoginState({ state: 'loading' }); // 👀 loading...
-  printLoginState({ state: 'success', response: { body: 'loaded' } }); // 😃 loaded
-  printLoginState({ state: 'fail', reason: 'no network' }); // 😱 no network
+  // Ellie
+  function printLoginState2(state: ResourceLoadState) {
+    switch (state.state) {
+      case "loading":
+        console.log("👀 loading...");
+        break;
+      case "success":
+        console.log(`😃 ${state.response.body}`);
+        break;
+      case "fail":
+        console.log(`😃 ${state.reason}`);
+        break;
+      default:
+        throw new Error(`unknown state : ${state}`);
+    }
+  }
+  printLoginState2({ state: "loading" }); // 👀 loading...
+  printLoginState2({ state: "success", response: { body: "loaded" } }); // 😃 loaded
+  printLoginState2({ state: "fail", reason: "no network" }); // 😱 no network
 }

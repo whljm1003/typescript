b9978c3085702e3105c54f2103813961837f5cdf
Jungmin Lee <whljm1003@gmail.com>
2022-06-16 15:05:42 +0900

    2-2 game

--- 2-types-projects-quiz/calculator.ts
+++ 2-types-projects-quiz/calculator.ts
@@ -2,12 +2,6 @@
   /**
    * Let's make a calculator 🧮
    */
-  console.log(calculate2("add", 1, 3)); // 4
-  console.log(calculate2("substract", 3, 1)); // 2
-  console.log(calculate2("multiply", 4, 2)); // 8
-  console.log(calculate2("divide", 4, 2)); // 2
-  console.log(calculate2("remainder", 5, 2)); // 1
-
   // My solution 😭
   type Command1 = "add" | "substract" | "multiply" | "divide" | "remainder";
   function calculate1(command: Command1, ...num: number[]): number {
@@ -44,4 +38,10 @@
         throw Error("unkown command");
     }
   }
+
+  console.log(calculate2("add", 1, 3)); // 4
+  console.log(calculate2("substract", 3, 1)); // 2
+  console.log(calculate2("multiply", 4, 2)); // 8
+  console.log(calculate2("divide", 4, 2)); // 2
+  console.log(calculate2("remainder", 5, 2)); // 1
 }
--- 2-types-projects-quiz/game.ts
+++ 2-types-projects-quiz/game.ts
@@ -1,12 +1,64 @@
-/**
- * Let's make a game 🕹
- */
-console.log(position); // { x: 0, y: 0}
-move('up');
-console.log(position); // { x: 0, y: 1}
-move('down');
-console.log(position); // { x: 0, y: 0}
-move('left');
-console.log(position); // { x: -1, y: 0}
-move('right');
-console.log(position); // { x: 0, y: 0}
+{
+  /**
+   * Let's make a game 🕹
+   */
+  const position: Position = {
+    x: 0,
+    y: 0,
+  };
+  interface Position {
+    x: number;
+    y: number;
+  }
+
+  // My solution
+  type Move = "up" | "down" | "left" | "right";
+  function move1(Moving: Move): void {
+    switch (Moving) {
+      case "up":
+        position.y++;
+        break;
+      case "down":
+        position.y--;
+        break;
+      case "left":
+        position.x--;
+        break;
+      case "right":
+        position.x++;
+        break;
+      default:
+        throw new Error("unkown move");
+    }
+  }
+
+  // Ellie
+  function move2(direction: "up" | "down" | "left" | "right") {
+    switch (direction) {
+      case "up":
+        position.y += 1;
+        break;
+      case "down":
+        position.y -= 1;
+        break;
+      case "left":
+        position.x -= 1;
+        break;
+      case "right":
+        position.x += 1;
+        break;
+      default:
+        throw new Error(`unkown direction: ${direction}`);
+    }
+  }
+
+  console.log(position); // { x: 0, y: 0}
+  move2("up");
+  console.log(position); // { x: 0, y: 1}
+  move2("down");
+  console.log(position); // { x: 0, y: 0}
+  move2("left");
+  console.log(position); // { x: -1, y: 0}
+  move2("right");
+  console.log(position); // { x: 0, y: 0}
+}

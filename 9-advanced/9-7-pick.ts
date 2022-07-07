f886691c6a3ff2def3495cb9bbc2f004318c028a
Jungmin Lee <whljm1003@gmail.com>
2022-07-07 14:49:44 +0900

    pick

---
+++ 9-advanced/9-7-pick.ts
@@ -0,0 +1,27 @@
+{
+  // Pick = 기존의 타입에서 원하는 속성과 값만 뽑아서 사용 가능함.
+
+  type Video = {
+    id: string;
+    title: string;
+    url: string;
+    data: string;
+  };
+
+  type VideoMetadata = Pick<Video, "id" | "title">;
+
+  function getVideo(id: string): Video {
+    return {
+      id,
+      title: "video",
+      url: "https//..",
+      data: "byte-data..",
+    };
+  }
+  function getVideoMetadata(id: string): VideoMetadata {
+    return {
+      id: id,
+      title: "title",
+    };
+  }
+}
